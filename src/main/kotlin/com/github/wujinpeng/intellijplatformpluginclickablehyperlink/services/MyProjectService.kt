42aebef4d311ece15e676658384f123741fd3be7
GitHub Action <action@github.com>
2023-02-06 11:54:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/wujinpeng/intellijplatformpluginclickablehyperlink/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wujinpeng/intellijplatformpluginclickablehyperlink/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wujinpeng.intellijplatformpluginclickablehyperlink.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wujinpeng.intellijplatformpluginclickablehyperlink.MyBundle
 
 class MyProjectService(project: Project) {
 
